72aeecba957768b29b1e517e14a0aa44033e8782
aferelay <sales@afe-relay.com>
2015-08-06 17:54:16 +0800

    add in database models

---
+++ aferelay/Models/Parts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aferelay.Models
+{
+    public class Parts
+    {
+        public int PartId { get; set; }
+
+    }
+}
